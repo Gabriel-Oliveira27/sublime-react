f02b2a1ac37ab2aadb28ab7f6a39dc3ffcab6ca0
Gabriel-Oliveira27 <mcpemaster620@gmail.com>
2026-05-21 00:52:53 -0300

    Atualização de ajuste de filtros e de configuração do prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,11 @@
-import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { neon } from '@neondatabase/serverless'
 
 const createPrisma = () => {
-  const sql = neon(process.env.DATABASE_URL!)
+  const url = process.env.DATABASE_URL
+  if (!url) throw new Error('DATABASE_URL não definida')
+  const sql = neon(url)
   const adapter = new PrismaNeon(sql as any)
   return new PrismaClient({ adapter } as any)
 }
